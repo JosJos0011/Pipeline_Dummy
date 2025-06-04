d8f23280cb70f90eb3245ce0f0e87a1047d16303
josjos0011 <josearmando.jimenez@ucr.ac.cr>
2025-06-03 18:02:53 -0600

    create duumy code for test

---
+++ test_primo.py
@@ -0,0 +1,14 @@
+import unittest
+from primo import es_primo
+
+class TestPrimo(unittest.TestCase):
+    def test_primos(self):
+        self.assertTrue(es_primo(2))
+        self.assertTrue(es_primo(3))
+        self.assertTrue(es_primo(11))
+        self.assertFalse(es_primo(1))
+        self.assertFalse(es_primo(4))
+        self.assertFalse(es_primo(9))
+
+if __name__ == '_main_':
+    unittest.main()
